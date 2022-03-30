fa060af3ea671ea6c96957add972868b8d9440dd
Alberto <albertopimentel04@gmail.com>
2022-03-30 17:53:40 -0600

    diseño y detalles por producto listo

---
+++ components/Guitarra.js
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Guitarra.module.css";
+function Guitarra({ guitarra }) {
+  const { nombre, imagen, precio, descripcion, url } = guitarra;
+  return (
+    <div className={styles.guitarra}>
+      <Image
+        priority="true"
+        layout="responsive"
+        width={180}
+        height={350}
+        src={imagen[0].url}
+        alt={`Imagen ${nombre}`}
+      />
+      <div className={styles.contenido}>
+        <h3>{nombre}</h3>
+        <p className={styles.descripcion}>{descripcion}</p>
+        <p className={styles.precio}>${precio}</p>
+        <Link href={`/guitarras/${url}`}>
+          <a className={styles.enlace}>Ver Producto</a>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Guitarra;
--- pages/tienda.js
+++ pages/tienda.js
@@ -1,18 +1,18 @@
 import Layout from "../components/Layout";
-
+import Listado from "../components/Listado";
 function Tienda({ guitarras }) {
-  console.log(guitarras);
   return (
     <Layout pagina="Tienda Virtual">
       <div className="contenedor">
         <h1 className="heading">Nuestra Coleccion</h1>
+        <Listado guitarras={guitarras} />
       </div>
     </Layout>
   );
 }
 
 export async function getServerSideProps() {
-  const url = `${process.env.API_URL}/guitarras`;
+  const url = `${process.env.API_URL}/guitarras?_sort=precio:asc`;
   const respuesta = await fetch(url);
   const guitarras = await respuesta.json();
   return {

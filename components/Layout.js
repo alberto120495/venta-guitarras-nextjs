645a0b874278f054b6008f30bb9a1599d4dd66bd
Alberto <albertopimentel04@gmail.com>
2022-03-31 12:28:22 -0600

    crossoorigin

--- components/Layout.js
+++ components/Layout.js
@@ -9,7 +9,7 @@ function Layout({ children, pagina, guitarra }) {
         <title>GuitarLA - {pagina}</title>
         <meta name="description" content="Sitio Web de venta de guitarras" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossoOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossoorigin />
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" />
       </Head>
       <Header guitarra={guitarra} />
